c15cfa9654dfcbfef956c00994394a463bb4bdcc
LuciaBortolozzi <lucia.bortolozzi@usal.edu.ar>
2020-08-08 17:25:02 -0300

    Initial Commit

--- src/model/DAO/PersonasTXT.java
+++ src/model/DAO/PersonasTXT.java
@@ -1,19 +1,19 @@
 package model.DAO;
 
-import model.Personas;
+import model.*;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.TreeSet;
+import java.util.*;
+
+import static controller.Validaciones.convertirAFechaCalendar;
 
 public class PersonasTXT {
 
-    private static final String directorio = "C:\\\\Users\\\\Flor\\\\IdeaProjects\\\\Guia-10\\\\src\\\\resources\\";
-//    private static final String directorio = "D:\\\\IdeaProjects\\\\Guia-10\\\\src\\\\resources\\\\";
+//    private static final String directorio = "C:\\\\Users\\\\Flor\\\\IdeaProjects\\\\Guia-10\\\\src\\\\resources\\\\";
+    private static final String directorio = "D:\\\\IdeaProjects\\\\Guia-10\\\\src\\\\resources\\\\";
 
-    public static TreeSet<Personas> bajarPersonasTXT() {
+    public static TreeSet<Personas> bajarPersonasTXT(ArrayList<Localidades> localidades, ArrayList<Hospitales> hospitales, ArrayList<TiposSangre> tiposSangre) {
 
         TreeSet<Personas> personas = new TreeSet<Personas>();
         try {
@@ -31,13 +31,63 @@ public class PersonasTXT {
                 // Guardar objetos
                 for (String s : personasST) {
 
-                    String[] personaST = s.split("\t");
+                    String[] personaST = s.split(";");
 
-                    String nombre = personaST[0].toUpperCase();
-                    String apellido = personaST[1].toUpperCase();
-                    char sexo = personaST[2].toUpperCase().charAt(0);
+                    int tipo = Integer.parseInt(personaST[0]);
+                    Calendar fechaSist = convertirAFechaCalendar(personaST[1]);
+                    String nombre = personaST[2].toUpperCase();
+                    String apellido = personaST[3].toUpperCase();
+                    long dni = Long.parseLong(personaST[4]);
+                    String localidadST = personaST[5].toUpperCase();
+                    Calendar fechaNac = convertirAFechaCalendar(personaST[6]);
+                    char sexo = personaST[7].toUpperCase().charAt(0);
+                    int tipoDeSangre = Integer.parseInt(personaST[8]);
 
-                    personas.add(new Personas());
+                    // Agregar localidad
+                    Localidades localidad = null;
+                    for (Localidades loc : localidades) {
+                        localidad = loc;
+                        if (localidad.getNombreLoc().equals(localidadST)) {
+                            break;
+                        }
+                    }
+
+                    // Agregar tipoSangre
+                    TiposSangre tipoSangre = null;
+                    for (TiposSangre tipoSang : tiposSangre) {
+                        tipoSangre = tipoSang;
+                        if (tipoSangre.getId() == tipoDeSangre) {
+                            break;
+                        }
+                    }
+
+                    if (tipo == 1) {
+                        String enfermedad = personaST[9].toUpperCase();
+                        int idHospital = Integer.parseInt(personaST[10]);
+                        Calendar inicioTratamiento = convertirAFechaCalendar(personaST[11]);
+
+                        // Agregar hospital
+                        Hospitales hospital = null;
+                        for (Hospitales hosp : hospitales) {
+                            hospital = hosp;
+                            if (hospital.getIdHospital() == idHospital) {
+                                break;
+                            }
+                        }
+
+                        personas.add(new Pacientes(nombre, apellido, dni, localidad, fechaNac, sexo, tipoSangre, enfermedad, hospital, inicioTratamiento));
+
+                    } else if (tipo == 2) {
+                        boolean donaSangre = Boolean.parseBoolean(personaST[9]);
+                        boolean donaPlaquetas = Boolean.parseBoolean(personaST[10]);
+                        boolean donaPlasma = Boolean.parseBoolean(personaST[11]);
+
+                        personas.add(new Donadores(nombre, apellido, dni, localidad, fechaNac, sexo, tipoSangre, donaSangre, donaPlaquetas, donaPlasma));
+
+                    } else {
+                        personas.add(new Personas(nombre, apellido, dni, localidad, fechaNac, sexo, tipoSangre));
+                    }
+
                 }
 
                 leerArchivoPersonas.close();
@@ -49,4 +99,5 @@ public class PersonasTXT {
 
         return personas;
     }
+
 }
